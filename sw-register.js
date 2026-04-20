68addd4af732622dc2b804a30efd2cb8b8352f91
sinvan4 <46162182+sinvan4@users.noreply.github.com>
2026-04-20 14:34:16 +0600

    sw-register.js

--- sw-register.js
+++ sw-register.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(reg => {
+        navigator.serviceWorker.register('sw.js').then(reg => {
             console.log('Service Worker registered');
         }).catch(err => {
             console.log('Service Worker registration failed:', err);
